b1ef44c7862664989499c62878bd1d9c58cab858
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-20 09:42:26 -0600

    added in a header element with a button that should render the village

---
+++ smurfs/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+//import action for getting data
+
+const SmurfHeader = props => {
+
+    const handleGetData = e => {
+        e.preventDefault();
+        props.getData();
+    };
+
+    return (
+
+        <div>
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          {props.isFetchingData ? (<div>How many Smurfs Can Live in a Village?</div>) : (<button onClick={handleGetData}>Check out the village!</button>)}
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        isFaetchingData: state.isFetchingData
+    }
+}
+
+export default connect(mapStateToProps, { getData })(SmurfHeader)
