ae1e0dbf0dd640d392fd6ed1d368cdf1e6a1fa23
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-20 09:28:06 -0600

    added in dependies as well as set up store and middleware in index.js. Also set up an inital state and skeleton reducer in smurfReducer.js

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,16 @@
+// import the actions
+
+const initialState = {
+    villiageData: [],
+    isFetchingData: false,
+    error: ''
+}
+
+export const smurfReducer = (state=initialState, action) => {
+    switch (action.type){
+
+
+        default:
+            return state;
+    }
+}
