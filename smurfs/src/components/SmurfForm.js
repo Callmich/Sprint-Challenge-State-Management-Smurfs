01685f13acb77573020ddde3f1487e50a6f4434f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-20 10:44:34 -0600

    setting up form currently the inputs set a component state that will be added into the array with the next axios request

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState}  from 'react';
 import { connect } from 'react-redux';
 
 const SmurfForm = props => {
 
+    const [newSmurf, setNewSmurf] = useState({
+        name: '',
+        age: '',
+        height: '',
+        id: ''
+    })
+
+    const handleChanges = e => {
+        setNewSmurf({ ...newSmurf, [e.target.name]: e.target.value });
+        console.log("New smurf info", newSmurf)
+    }
+
 
     return (
         <div>
-            <h2>Here is where the form will go</h2>
+          <form>
+            <input 
+              label="Name"
+              id="name"
+              name="name"
+              type="text"
+              value={newSmurf.name}
+              onChange={handleChanges}
+              placeholder="Name"
+              />
+            <input 
+              label="Age"
+              id="age"
+              name="age"
+              type="number"
+              value={newSmurf.age}
+              onChange={handleChanges}
+              placeholder="Age"
+              />
+            <input 
+              label="Height"
+              id="height"
+              name="height"
+              type="text"
+              value={newSmurf.height}
+              onChange={handleChanges}
+              placeholder="Height"
+              />
+          </form>
         </div>
     )
 }
 
-export default connect( ()=>{}, {} )(SmurfForm)
+const mapStateToProps = state => {
+    return {
+        villageData: state.villageData,
+    }
+}
+
+export default connect( mapStateToProps, {} )(SmurfForm)
